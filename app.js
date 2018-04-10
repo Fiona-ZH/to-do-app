1207eb6ee386ca2876410e3ace028d37ecacb4f5
fiona <fiona.feifei.zhang@gmail.com>
2018-04-10 18:16:39 -0400

    assignment 5 DOMscripting-1 completed

--- app.js
+++ app.js
@@ -8,20 +8,39 @@ function onReady() {
     let title = newToDoText.value;
     //create a new li
     let newLi = document.createElement('li');
+    newLi.className = 'mdl-list__item';
     //create a new input
     let checkbox = document.createElement('input');
     //set the input's type to checkbox
     checkbox.type = "checkbox";
+    checkbox.className = 'mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect mdl-checkbox__input';
+    //create a delete button
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete";
+    deleteButton.className = 'mdl-button mdl-js-button mdl-js-ripple-effect';
     //set the title
     newLi.textContent = title;
     //attach the checkbox to the li
     newLi.appendChild(checkbox);
+    //attach the deleteButton to the li
+    newLi.appendChild(deleteButton);
     //attach the li to the ul
     toDoList.appendChild(newLi);
     //empty the input
     newToDoText.value = '';
-    
+
+    //remove li by clicking the delete button
+    deleteButton.onclick = function(){
+      toDoList.removeChild(newLi);
+    }
+    newToDoText.focus();
   });
+
+}
+
+function deleteLi() {
+    const dBtn = document.getElementById('dBtn');
+
 }
 
 window.onload = function() {
