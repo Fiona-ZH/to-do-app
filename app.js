607ed95301cb608078f37655bac79aa74d146cbb
fiona <fiona.feifei.zhang@gmail.com>
2018-04-12 11:47:43 -0400

    assignment-6-DOMScripting-2 completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function onReady() {
-  const toDos = [];
+  let iD = 0;
+  var toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
 
   function createNewToDo() {
@@ -10,30 +11,48 @@ function onReady() {
 
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: iD
     });
+
+    iD++;
+
     newToDoText.value = '';
     newToDoText.focus();
 
     renderTheUI();
   }
 
-    function renderTheUI(){
+  function renderTheUI(){
       const toDoList = document.getElementById('toDoList');
 
       toDoList.textContent = '';
 
       toDos.forEach(function(toDo){
         const newLi = document.createElement('li');
+        newLi.textContent = toDo.title;
+
         const checkbox = document.createElement('input');
         checkbox.type = "checkbox";
-        newLi.textContent = toDo.title;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = "Delete";
+
+        function deleteLi(){
+          toDos = toDos.filter(function(todo){
+            return toDo.id != todo.id;
+          });
+          renderTheUI();
+        }
+        deleteButton.addEventListener('click', deleteLi);
 
         toDoList.appendChild(newLi);
         newLi.appendChild(checkbox);
+        newLi.appendChild(deleteButton);
+
       });
 
-    }
+  }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
